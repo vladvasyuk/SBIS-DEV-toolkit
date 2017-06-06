1f41d927f9f847ad0061d2a8d4cb5b3da0cd991f
vladvasyuk <va.vasuk@r78-vasuknix.region.tensor.ru>
2017-06-06 13:35:45 +0300

    Bump version

--- python_tools/setup.py
+++ python_tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='SBISToolkit',
-      version='1.1',
+      version='1.3',
       description='SBIS Python dev toolkit.',
       author='Vladislav Vasyuk',
       author_email='nazgarth@gmail.com',
